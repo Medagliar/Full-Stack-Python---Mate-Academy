a74a3f63d20362b2b03c2321cfc156ec1dbe51a4
Rafael Medaglia <120750717+Medagliar@users.noreply.github.com>
2025-06-12 21:41:51 +0000

    checa se posso fazer chá

--- Python Basics/Boolean/make_tea.py
+++ Python Basics/Boolean/make_tea.py
@@ -7,11 +7,8 @@
 #Altere is_water_hot ou have_tea para que can_make_tea seja False.
 
 # change code below this line
-
 is_water_hot = True
 have_tea = False
-
 # change code above this line
-
 can_make_tea = is_water_hot and have_tea  # don't change this line
 print(can_make_tea)
